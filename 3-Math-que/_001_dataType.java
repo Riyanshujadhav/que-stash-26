a220a8abf4dcb4a63e9c3f2a2f70c269b5b23048
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-12 02:36:55 +0530

    data_type

---
+++ 3-Math-que/_001_dataType.java
@@ -0,0 +1,23 @@
+
+
+public class _001_dataType {
+    public static void main(String[] args) {
+        byte  a= 127;
+        short b= 32767;
+        int   c= 2_147_483_647;
+        long  d= 923456789l;
+        double  e=3.142857142857142857;
+        float   f= 3.14285714f;
+        boolean t=true;
+        char    ch='s';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(t);
+        System.out.println(ch);
+    }
+    
+}
