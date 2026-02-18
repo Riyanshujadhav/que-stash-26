aa625ac657b93dfeb32ca74f42004eec52bf2d8d
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-02-19 00:12:42 +0530

    armstrong-no

--- 3-Math-que/_023_armStrongNo.java
+++ 3-Math-que/_023_armStrongNo.java
@@ -2,8 +2,30 @@
 import java.util.Scanner;
 
 public class _023_armStrongNo {
+    public static int digit(int n){
+        int digits=0;
+        while (n!=0){
+            int temp=n%10;
+            digits++;
+            n/=10;
+        }return digits;
+    }
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        int num=n;
+        int sum=0;
+        int digit=digit(n);
+        while (n!=0){
+            int temp=n%10;
+            sum=sum+(int)Math.pow(temp,digit);
+            n/=10;
+        }
+        if(sum==num){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
     }
 }
