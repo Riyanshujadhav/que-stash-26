414c86cb9157b8eca814d75a5c5a93dbfa18b6b2
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-04 08:23:31 +0530

    assigment-3-math

--- 3-Math-que/_039_15que.java
+++ 3-Math-que/_039_15que.java
@@ -13,6 +13,37 @@
 
 // Sample Output
 // 11011
+
+import java.util.Scanner;
+
 public class _039_15que {
-    
+    public static int todecimal(int sb,int sn){
+		int decimal=0;
+		int pow=1;
+		while(sn>0){
+			int temp=sn%10;
+			decimal=decimal+temp*pow;
+			pow=pow*sb;
+			sn/=10;
+		}return decimal;
+	}
+	public static int todb(int db,int decimal){
+		int pow=1;
+		int ans=0;
+		while(decimal>0){
+			int temp=decimal%db;
+			ans=ans+temp*pow;
+			pow=pow*10;
+			decimal/=db;
+		}return ans;
+	}
+    public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		int sb=sc.nextInt();
+		int db=sc.nextInt();
+		int sn=sc.nextInt();
+		int decimal=todecimal(sb,sn);
+		int ans= todb(db,decimal);
+		System.out.println(ans);
+    }
 }
