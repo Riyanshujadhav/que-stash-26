45fc9f9651b1bd1a10a13fb3d815e3a077326242
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-05 09:12:23 +0530

    assigment-4-array

--- 5-ArrayList-que/_01_Assigment4_.java
+++ 5-ArrayList-que/_01_Assigment4_.java
@@ -2,10 +2,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class _01_Assigment4_ {
-    public static void main(String args[]) {
+        public static void main(String args[]) {
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
         ArrayList<Integer> list= new ArrayList<>();
+    
         ArrayList<Integer>ans=new ArrayList<>();
         for (int i = 0; i < n; i++) {
             list.add(sc.nextInt());
