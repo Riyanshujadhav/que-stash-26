0b2e8be529685ead30ea43356d4cee8dcabff83f
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-06 23:05:50 +0530

    Assigment-4-array

---
+++ 5-ArrayList-que/_03_Assigment4_.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class _03_Assigment4_ {
+    public static int findIndex(int []arr,int m){
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == m) {
+                return i;
+            }
+        }
+        return -1;
+	}
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+		int m = sc.nextInt();
+        int ans = findIndex(arr, m);
+        System.out.println(ans);
+    }
+}
