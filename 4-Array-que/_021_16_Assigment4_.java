4b9b414077b0f866b05ab01884948e8aff1ea76c
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-12 06:06:03 +0530

    Assigment-4

--- 4-Array-que/_021_16_Assigment4_.java
+++ 4-Array-que/_021_16_Assigment4_.java
@@ -1,3 +1,27 @@
+// InsertionSort
+import java.util.Scanner;
 public class _021_16_Assigment4_ {
-    
+    public static void insertionSort(int[] arr, int n) {
+        for(int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        insertionSort(arr, n);
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
