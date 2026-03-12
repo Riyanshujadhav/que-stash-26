6ceb238b067b1223dc976e68bb597207d307c245
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-13 02:21:28 +0530

    Assigment-4

--- 4-Array-que/_029_24_Assigment4_.java
+++ 4-Array-que/_029_24_Assigment4_.java
@@ -1,3 +1,7 @@
+// You are given an array of integers that represents a permutation of numbers. 
+// Your task is to find the next lexicographical permutation of the array. 
+// If such a permutation does not exist (i.e., the array is sorted in descending order), 
+// rearrange the array to its smallest permutation (sorted in ascending order).
 public class _029_24_Assigment4_ {
     
 }
