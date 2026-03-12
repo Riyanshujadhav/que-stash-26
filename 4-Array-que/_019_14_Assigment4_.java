3b11ff6bf908a9da3287f12ea5bd58fc9a060a9a
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-12 06:05:44 +0530

    Assigment-4

--- 4-Array-que/_019_14_Assigment4_.java
+++ 4-Array-que/_019_14_Assigment4_.java
@@ -1,3 +1,7 @@
+// You are provided two sorted arrays. 
+// You need to find the sum of the maximum sum path to reach from beginning of any array to end of any of the two arrays. 
+// You can switch from one array to another array only at common elements.
+
 public class _019_14_Assigment4_ {
     
 }
