29a6f0b4a909e1c3a4915d249dd3b4b37d38a042
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-12 06:05:23 +0530

    Assigment-4

--- 4-Array-que/_017_12_Assigment4_.java
+++ 4-Array-que/_017_12_Assigment4_.java
@@ -1,3 +1,13 @@
+// Raj is a very smart kid who recently started learning computer programming.
+// His coach gave him a cyclic array A having N numbers, and he has to perform Q operations on this array. 
+// In each operation the coach would provide him with a number X. 
+// After each operation, every element of the cyclic array would be replaced by the sum of itself 
+// and the element lying X positions behind it in the cyclic array. All these replacements take place simultaneously. 
+// For example, if the cyclic array was [a, b, c, d], then after the operation with X = 1, 
+// the new array would be [a+d, b+a, c+b, d+c]. He needs to output the sum of the elements 
+// of the final array modulus 10^9+7. He made a program for it but it's not very efficient. 
+// You know he is a beginner, so he wants you to make an efficient program for this task because 
+// he doesn't want to disappoint his coach.
 public class _017_12_Assigment4_ {
     
 }
