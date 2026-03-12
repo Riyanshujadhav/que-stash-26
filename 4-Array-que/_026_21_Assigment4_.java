b1da3fe444c3cf60fc6a0c8dfe28c9fff8525e4b
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-13 02:20:50 +0530

    Assigment-4

--- 4-Array-que/_026_21_Assigment4_.java
+++ 4-Array-que/_026_21_Assigment4_.java
@@ -1,3 +1,15 @@
+// Given a binary number ,help Von Neuman to find out its decimal representation. 
+// For eg 000111 in binary is 7 in decimal.
+import java.util.Scanner;
+
 public class _026_21_Assigment4_ {
-    
+    public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            String binary = sc.next();
+            int decimal = Integer.parseInt(binary, 2);
+            System.out.println(decimal);
+        }
+    }
 }
