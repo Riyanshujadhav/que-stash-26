1b8cba448181cda2f948b057330f03bb3d96482d
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-13 02:20:12 +0530

    Assigment-4

--- 4-Array-que/_025_20_Assigment4_.java
+++ 4-Array-que/_025_20_Assigment4_.java
@@ -1,3 +1,21 @@
+// Given an array nums of length n. We define a running sum of an array 
+// as for every index runningSum[i] = sum(nums[0]…nums[i]).
+// Return the running sum of array for each i (0 <= i < n).
+
+import java.util.Scanner;
+
 public class _025_20_Assigment4_ {
-    
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            System.out.print(sum + " ");
+        }
+   } 
 }
