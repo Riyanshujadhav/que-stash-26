a688676e916473de783074108c566a05c81c784a
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-13 06:37:47 +0530

    Leetcode

---
+++ 6-Leetcode-que/_15_leetcode.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _15_leetcode {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+    List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0; i < nums.length - 2; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while(left < right && nums[left] == nums[left-1]) left++;
+                    while(left < right && nums[right] == nums[right+1]) right--;
+                }
+                else if(sum < 0){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+        }
+    return ans;
+    }
+
+    // O(N^3) -->
+    //
+    // public List<List<Integer>> threeSum(int[] nums) {
+    // Arrays.sort(nums);
+    // List<List<Integer>> ans = new ArrayList<>();
+    //     for(int i = 0; i < nums.length - 2; i++){
+    //         if(i > 0 && nums[i] == nums[i-1])
+    //             continue;
+    //         for(int j = i + 1; j < nums.length - 1; j++){
+    //             if(j > i + 1 && nums[j] == nums[j-1])
+    //                 continue;
+    //             for(int k = j + 1; k < nums.length; k++){
+    //                 if(k > j + 1 && nums[k] == nums[k-1])
+    //                     continue;
+    //                 if(nums[i] + nums[j] + nums[k] == 0){
+    //                     ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+    //                 }
+    //             }
+    //         }
+    //     }
+    // return ans;
+}
