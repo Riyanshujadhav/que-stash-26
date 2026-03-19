d0a9bfa5348b8798f188e78e80cc961b31802944
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-20 00:48:54 +0530

    basic-assigment1

--- 1-Basic-que/_0022AssigmentQue9.java
+++ 1-Basic-que/_0022AssigmentQue9.java
@@ -3,6 +3,18 @@
 
 // Input Format
 // A single line containing two space-separated integers, $A$ and $B$.
+
+import java.util.Scanner;
+
 public class _0022AssigmentQue9 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        if(n==m)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
     
 }
