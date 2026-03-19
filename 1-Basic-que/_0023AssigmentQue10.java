385533e44c64f452f6a42fface725d77c672f8b0
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-20 00:49:06 +0530

    basic-assigment1

--- 1-Basic-que/_0023AssigmentQue10.java
+++ 1-Basic-que/_0023AssigmentQue10.java
@@ -3,6 +3,24 @@
 
 // Input Format
 // A single line containing three space-separated integers: A, B, and C.
+
+import java.util.Scanner;
+
 public class _0023AssigmentQue10 {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+        if (a>b) {
+            if (a>c) {
+                System.out.println(a);
+            } else {System.out.println(c);
+            }
+            
+        } else if(b>c) {
+            System.out.println(b);
+        }else{System.out.println(c);}
+
+    }
 }
