89123f89bde877508688347b370f1e13b01a5a4b
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-20 00:48:06 +0530

    basic-assigment1

--- 1-Basic-que/_0018AssigmentQue5.java
+++ 1-Basic-que/_0018AssigmentQue5.java
@@ -1,4 +1,12 @@
 //Write a program that takes an integer as input and checks whether the number is zero or non-zero.
+import java.util.Scanner;
 public class _0018AssigmentQue5 {
-    
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n==0)
+            System.out.println("no is zero ");
+        else 
+            System.out.println("no is non-zero");
+    }
 }
