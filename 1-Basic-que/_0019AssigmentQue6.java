ffb4f49bcf82e15c9694cf33bdf6e9c3162602ee
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-20 00:48:20 +0530

    basic-assigment1

--- 1-Basic-que/_0019AssigmentQue6.java
+++ 1-Basic-que/_0019AssigmentQue6.java
@@ -1,4 +1,12 @@
 //Write a program that takes an integer as input and checks whether the number is strictly greater than 100.
+import java.util.Scanner;
 public class _0019AssigmentQue6 {
-    
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>100)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
 }
