b803469e45bfbf3d6c9a97e214da477a66c38325
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-20 00:49:19 +0530

    basic-assigment1

--- 1-Basic-que/_0024AssigmentQue11.java
+++ 1-Basic-que/_0024AssigmentQue11.java
@@ -8,8 +8,21 @@
 
 // Marks less than 60: Fail
 
-
+import java.util.Scanner;
 
 public class _0024AssigmentQue11 {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int grade= sc.nextInt();
+        if (grade>=90) {
+            System.out.println("A");
+        } else if (grade>=75) {
+            System.out.println("B");
+        } else if (grade>=60) {
+            System.out.println("C");
+        } else {
+            System.out.println("Fail");
+        }
+
+    }
 }
