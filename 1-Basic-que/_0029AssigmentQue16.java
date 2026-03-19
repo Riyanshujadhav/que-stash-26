81cc0884a006bf4cfe2eefc958376edfe31e1c29
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-20 00:50:23 +0530

    basic-assigment1

--- 1-Basic-que/_0029AssigmentQue16.java
+++ 1-Basic-que/_0029AssigmentQue16.java
@@ -5,6 +5,21 @@
 // Isosceles: Exactly two sides are equal.
 
 // Scalene: All three sides are different.
+
+import java.util.Scanner;
+
 public class _0029AssigmentQue16 {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+        if(a==b&&b==c){
+            System.out.println("equilateral");
+        }else if(a==b&&b!=c||b==c&&a!=b){
+            System.out.println("isosceles");
+        }else{
+            System.out.println("scalene");
+        }
+    }
 }
