4fd5eb0639f137369ba243d4bd6daa386f7dbd32
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-31 03:54:56 +0530

    leetcode

---
+++ 6-Leetcode-que/_1991_leetcode.java
@@ -0,0 +1,16 @@
+public class _1991_leetcode {
+    public int findMiddleIndex(int[] nums) {
+       int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        } 
+        int leftsum=0;
+        for(int i=0;i<nums.length;i++){
+            int rightsum=sum-leftsum-nums[i];
+            if(leftsum==rightsum){
+                return i;
+            }
+            leftsum+=nums[i];
+        }return -1; 
+    }
+}
