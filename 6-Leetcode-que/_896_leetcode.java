a3d726392af7eb0f80f0e5eb2667d6daea18c92c
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-30 11:30:34 +0530

    leetcode

---
+++ 6-Leetcode-que/_896_leetcode.java
@@ -0,0 +1,18 @@
+public class _896_leetcode {
+    public boolean isMonotonic(int[] nums) {
+        boolean inc=true;
+        boolean dec=true;
+        for(int i=1;i<nums.length;++i){
+            if(nums[i]> nums[i-1]){
+                inc=false;
+            }else if(nums[i]< nums[i-1]){
+                dec=false;
+            }
+        }
+        if(!inc && !dec){
+            return false;
+        }else {
+            return true;
+        }
+    }
+}
