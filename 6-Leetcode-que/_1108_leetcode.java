eb2bdbf9d7551b546d15d06707e5189924ce7c47
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-03-31 05:24:49 +0530

    leetcode

---
+++ 6-Leetcode-que/_1108_leetcode.java
@@ -0,0 +1,15 @@
+public class _1108_leetcode {
+    public String defangIPaddr(String address) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < address.length(); i++) {
+            if (address.charAt(i) == '.') {
+                result.append("[.]");
+            } else {
+                result.append(address.charAt(i));
+            }
+        }
+
+        return result.toString();
+    }
+}
