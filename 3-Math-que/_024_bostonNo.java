351c4b83a5beda279e4459f6ba859ad59af968c9
Riyanshujadhav <165128487+Riyanshujadhav@users.noreply.github.com>
2026-04-06 01:13:44 +0530

    bostanNo

--- 3-Math-que/_024_bostonNo.java
+++ 3-Math-que/_024_bostonNo.java
@@ -15,7 +15,7 @@ public class _024_bostonNo {
         if(n<=1){
             return false;
         }
-        for(int i=2;i<Math.sqrt(n);i++){
+        for(int i=2;i<=Math.sqrt(n);i++){
             if(n%i==0){
                 return false;
             }
